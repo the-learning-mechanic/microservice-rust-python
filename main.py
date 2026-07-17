9ca38615c7b5fb208c9d19139e73ddc33c0eb99a
the-learning-mechanic <manny.pardo@gmail.com>
2026-07-17 14:07:13 -0400

    reverting

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World Version Tres"}
+    return {"Hello": "World Revert Too Two"}
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
